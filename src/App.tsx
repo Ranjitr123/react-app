b3778a293c89c04d6869167b9e653b7f90c97fb6
Ranjit Rautaray <ranjit.r@terralogic.com>
2023-06-16 14:47:50 +0530

    third commited msg

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,50 @@ import './App.scss';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import axios from 'axios';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
+import Products from './components/products';
+import Nopagefound from "./components/page-not-found"
 
 
 function App() {
 
 const [state,setState] =  useState(0);
+const [value,setValue ] = useState([])
 let url = "https://jsonplaceholder.typicode.com/posts"
 useEffect(()=>{
 
   axios.get(url).then((res)=>{
   console.log(res.data)
+  setValue(res.data)
   }).catch((err)=>{
-    console.log(err)
+    console.log(err.message)
   })
 },[])
   return (
     <div className="App">
-     <h1 className='text-center text-success'>this is heading part</h1>
+     {/* <h1 className='text-center text-success'>this is heading part</h1>
      <h2>{state}</h2>
      <button onClick={()=>setState(state+1)}>increase</button>
-     <button onClick={()=>setState(state-1)}>decrease</button>
+     <button onClick={()=>state===0 ? setState(0):setState(state-1)}>decrease</button>
+
+     {
+      value.map((val:any)=>
+        <p key={val.id} className='text-center text-success'>{val.title}</p>
+        )
+     } */}
+
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/about' element={<About />}></Route>
+      <Route path='/contact' element={<Contact />}></Route>
+      <Route path='/products' element={<Products />}></Route>
+      <Route path='*' element={<Nopagefound />}></Route>   
+     </Routes>    
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../scss/about.scss"
+
+function About() {
+  return (
+    <div>
+        <h1 className='text-center'>About</h1>
+    </div>
+  )
+}
+
+export default About
