2875d2106806b26f0ad5170408a28a746660eb4d
Ranjit Rautaray <ranjit.r@terralogic.com>
2023-06-16 11:48:54 +0530

    second committed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js"
+import "bootstrap/dist/js/bootstrap.js";
+import axios from 'axios';
+
 
 function App() {
+
+const [state,setState] =  useState(0);
+let url = "https://jsonplaceholder.typicode.com/posts"
+useEffect(()=>{
+
+  axios.get(url).then((res)=>{
+  console.log(res.data)
+  }).catch((err)=>{
+    console.log(err)
+  })
+},[])
   return (
     <div className="App">
      <h1 className='text-center text-success'>this is heading part</h1>
+     <h2>{state}</h2>
+     <button onClick={()=>setState(state+1)}>increase</button>
+     <button onClick={()=>setState(state-1)}>decrease</button>
     </div>
   );
 }
