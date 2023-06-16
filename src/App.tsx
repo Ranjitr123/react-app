22ed84f6f35811c3f20725fed640793f3b72c988
Ranjit Rautaray <ranjit.r@terralogic.com>
2023-06-16 11:07:06 +0530

    fist commited msg

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js"
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-center text-success'>this is heading part</h1>
+    </div>
+  );
+}
+
+export default App;
